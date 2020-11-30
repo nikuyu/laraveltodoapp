fa8a2adcdd8dc38140437eccb65379ffe8d2dbc1
arifmustaqim <nikuyu91@gmail.com>
2020-11-30 15:20:18 +0800

    change laravel to arif mustaqim

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Arif Mustaqim
                 </div>
 
                 <div class="links">
